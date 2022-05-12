5c265362c4cb3acf0fdf9c4f535c70942e58ac7b
08laurar <laurarlmmamani301@gmail.com>
2022-05-12 18:00:07 +0000

    tareaejemplo

--- estrepetitivas/ejercicios_RR.java
+++ estrepetitivas/ejercicios_RR.java
@@ -1,43 +1,64 @@
 package estrepetitivas;
 
+import java.util.Scanner;
+
 /**
- * ejercicios_RR
+ * EjercicioR
  */
 public class ejercicios_RR {
 
-    public static void imprimirWhileN1_21() {
-    int  numInit=21;
-    while (numInit<=21) {
-         System.out.println(numInit);
-         numInit=numInit+1; 
-        }    
-    }
+    public static void inprimirwhileN1_10() {
+        int numInit=21;
+        while(numInit<=20) {
+           System.out.println(numInit);
+           numInit=numInit+1;
+        }
 
-    public static void imprimirDowhileN1_21() {
+    }
+    public static void inprimirDowhileN1_20() {
         int numInit=21;
         do {
             System.out.println(numInit);
             numInit++;
         } while (numInit<=20);
-
-
-    }  
-
-    public static void  imprimirFordN1_20() {
+        
+    }
+    public static void inprimirForN1_20() {
         for (int numInit = 1; numInit <= 20; numInit++) {
             System.out.println(numInit);
             
-        } 
-    
+        }
         
     }
     
-     }
-
-    public static void main(String[] args) {
-       System.out.println("Imprime While:"); imprimirWhileN1_21();  
-       System.out.println("Imprime Do while:"); imprimirDowhileN1_21();
-
+    
+    public static void nombrevertical() {
+        Scanner lt=new Scanner(System.in);
+        System.out.println("ingrese su nombre");
+        String nombre=lt.next();
+        for (int i= 0; i < nombre.length(); i++) {
+            System.out.println(nombre.charAt(i));
+            
+        }
+        
+    }
+    public void nombrealovolteado() {
+        Scanner lt=new Scanner(System.in);
+        System.out.println("ingrese su erbmon");
+        String erbmon=lt.next();
+        for (int i = erbmon.length()-1; i >=0; i--){
+            System.out.println(erbmon.charAt(i));
+        }
+        
     }
 
+    public static void main(String[] args) {
+       /*System.out.println("inprimir while"); inprimirwhileN1_10();
+       System.out.println("imprimir do while");inprimirDowhileN1_20();
+       System.out.println("inprimir for");inprimirForN1_20();*/
+
+       nombrevertical();
+
+        
+    }
 }
