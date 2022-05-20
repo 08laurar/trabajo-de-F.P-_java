a4a5fbf30e0a8feac53914a580f43b31a0f6c88f
08laurar <laurarlmmamani301@gmail.com>
2022-05-20 21:07:09 +0000

    ejercicios de programacion de reforzamiento

--- reforzamiento2022g1/ejercicio1.java
+++ reforzamiento2022g1/ejercicio1.java
@@ -11,25 +11,26 @@ public class ejercicio1 {
        //definir variable
        int diasem; 
        String mensaje;
+       String primeraParteM="ese dia  toca el curso de: ";
        //Leer datos
        Scanner leerT=new Scanner(System.in);
        System.out.println("ingrese un dia de la semana:\n1=Dom"+"\n2=Lun\n3=Mar\n4=mier\n5=jue\n6=vier\n7=sab");
        diasem=leerT.nextInt();
        //Proceso
        if (diasem==1) {
-           mensaje="No Hay Clases";
+           mensaje=primeraParteM+"No Hay Clases";
        }else if(diasem==2) {
-             mensaje="matematica";
+             mensaje=primeraParteM+"matematica";
        }else if(diasem==3) {
-             mensaje="fundamentos de programacion";
+             mensaje=primeraParteM+"fundamentos de programacion";
        }else if(diasem==4) {
-             mensaje="capacidades comonucativas"; 
+             mensaje=primeraParteM+"capacidades comonucativas"; 
        }else if (diasem==5) {
-             mensaje="introduccion ala ingeneria";
+             mensaje=primeraParteM+"introduccion ala ingeneria";
        }else if (diasem==6) {
-              mensaje="formacion cristiana";
+              mensaje=primeraParteM+"formacion cristiana";
        }else if (diasem==7) {
-              mensaje="no hay clases"; 
+              mensaje=primeraParteM+"no hay clases"; 
             }else{
                 mensaje="el dia  que  coloco no  existe";
             }
